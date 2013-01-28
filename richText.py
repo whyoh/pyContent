8658655ce26e00d519a151dfb3abfacdcccea3fb
whyoh <github@entrophilia.com>
2013-01-28 11:51:23 +0000

    Update richText.py
    obsolete _tidy method, correct delete method.
    allow passing text to init and additional properties to add.

--- richText.py
+++ richText.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# richText class manages a block of text and a list of items with ranges of the text to which they apply
+# richText class manages a block of text and a list of items with ranges of the text to which they apply.
 # the items are dicts so you can add your own properties to them.  'name', 'start' and 'end' are reserved.
-# items may cover zero or more characters within the text (if you delete all the text in an item you'll leave an empty item - it's not removed.  subclasses may do such removals)
+# items may cover zero or more characters within the text.
+# if you delete all the text in an item you'll leave an empty item - it's not removed.  subclasses may do such removals
 
 # TODO could use a networkx network instead of a list of dicts for the items for added functionality
 
+from copy import deepcopy
+
 class richText:
 
-  def __init__(self):
+	def __init__(self, text = ""):
 		self._styles = []
-		self._text = ""
+		self._text = text
 	
-	def _tidy(self):
-		toRemove = []
-		for n, style in enumerate(self._styles):
-			if style['start'] >= style['end']: toRemove.append(n)
-		for n in reversed(toRemove): del self._styles[n]
-
-	def add(self, tag, start, end):
-		assert start < end
+	def add(self, tag, start, end, **kwargs):
+		assert start <= end
 		assert end <= len(self._text)
-		self._styles.append({'name': tag, 'start': start, 'end': end})
-
+		item = deepcopy(kwargs)
+		item['name'] = tag
+		item['start'] = start
+		item['end'] = end
+		self._styles.append(item)
+	
 	def removeAll(self, tag):
 		toRemove = list(m for m, style in enumerate(self._styles) if style['name'] == tag)
 		for m in reversed(toRemove): del self._styles[m]
@@ -49,9 +50,6 @@ class richText:
 		assert start <= end
 		self._text = self._text[:start] + self._text[end:]
 		removed = end - start
-		toRemove = []
 		for n, style in enumerate(self._styles):
 			if style['start'] > start: style['start'] = max(start, style['start'] - removed)
 			if style['end'] > start: style['end'] = max(start, style['end'] - removed)
-			if style['start'] == style['end']: toRemove.append(n)
-		for n in reversed(toRemove): del self._styles[n]
